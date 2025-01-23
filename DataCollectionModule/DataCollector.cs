d30fa70f2ef4ab2db81c2679031dfa09bbb6384a
Benjamin Evans <51272212+dfgHiatus@users.noreply.github.com>
2025-01-23 12:23:18 -0600

    (tweak) Make StringBuilder non-static

--- DataCollectionModule/DataCollector.cs
+++ DataCollectionModule/DataCollector.cs
@@ -9,14 +9,14 @@ namespace DataCollectionModule;
 
 public class DataCollector : ExtTrackingModule
 {
+    private static readonly UnifiedExpressions[] AllExpressions = Enum.GetValues<UnifiedExpressions>();
+
     private CsvConfig _config;
     private StreamWriter? _writer;
     private Thread? _collectionThread;
+    private StringBuilder? _sb;
     private bool _isRunning = true;
-
-    private static readonly UnifiedExpressions[] AllExpressions = Enum.GetValues<UnifiedExpressions>();
-    private static readonly StringBuilder sb = new StringBuilder();
-
+    
     public override (bool SupportsEye, bool SupportsExpression) Supported => (true, true);
 
     public override (bool eyeSuccess, bool expressionSuccess) Initialize(bool eyeAvailable, bool expressionAvailable)
@@ -40,6 +40,7 @@ public class DataCollector : ExtTrackingModule
             File.WriteAllText(configPath, emptyConfigJson);
         }
 
+        _sb = new StringBuilder();
         _config = JsonConvert.DeserializeObject<CsvConfig>(File.ReadAllText(configPath))!;
         _writer = new StreamWriter(Path.Combine(path, _config.FilePath), _config.Append);
         _writer.WriteLine(string.Join(',', AllExpressions) + ",EyeImageX,EyeImageY,EyeImageData,FaceImageX,FaceImageY,FaceImageData");
@@ -78,42 +79,42 @@ public class DataCollector : ExtTrackingModule
         var eyeImage = UnifiedTracking.EyeImageData;
         var faceImage = UnifiedTracking.LipImageData;
 
-        sb.Clear();
+        _sb?.Clear();
         if (_config.ExpressionFilter.Count > 0)
         {
             foreach (var key in _config.ExpressionFilter)
             {
-                sb.Append($"{expressions[(int)key].Weight},");
+                _sb?.Append($"{expressions[(int)key].Weight},");
             }
         }
         else
         {
             foreach (var key in AllExpressions)
             {
-                sb.Append($"{expressions[(int)key].Weight},");
+                _sb?.Append($"{expressions[(int)key].Weight},");
             }
         }
 
         if (_config.CaptureEyeImages && eyeImage.SupportsImage)
         {
-            sb.Append($"{eyeImage.ImageSize.x},{eyeImage.ImageSize.y},{Convert.ToBase64String(eyeImage.ImageData)}");
+            _sb?.Append($"{eyeImage.ImageSize.x},{eyeImage.ImageSize.y},{Convert.ToBase64String(eyeImage.ImageData)}");
         }
         else
         {
-            sb.Append("-1,-1,-1,");
+            _sb?.Append("-1,-1,-1,");
         }
 
         if (_config.CaptureFaceImages && faceImage.SupportsImage)
         {
-            sb.Append($"{faceImage.ImageSize.x},{faceImage.ImageSize.y},{Convert.ToBase64String(faceImage.ImageData)},");
+            _sb?.Append($"{faceImage.ImageSize.x},{faceImage.ImageSize.y},{Convert.ToBase64String(faceImage.ImageData)},");
         }
         else
         {
-            sb.Append("-1,-1,-1,");
+            _sb?.Append("-1,-1,-1,");
         }
 
         // Write the collected data as a single line
-        _writer?.WriteLine(sb.ToString().TrimEnd(','));
+        _writer?.WriteLine(_sb?.ToString().TrimEnd(','));
     }
 
     public override void Update()
